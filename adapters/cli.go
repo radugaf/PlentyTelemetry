8667ab7eae8e6b19bcbe5c2b886ccb378706cb65
Radu Gafita <radugaf@gmail.com>
2025-06-02 19:48:31 +0300

    Added a test backend + did cli driver adapter

--- adapters/cli.go
+++ adapters/cli.go
@@ -2,6 +2,7 @@ package adapters
 
 import (
 	"fmt"
+	"strings"
 
 	p "github.com/radugaf/PlentyTelemetry/ports"
 )
@@ -13,10 +14,21 @@ func NewCLIDriver() *CLIDriver {
 }
 
 func (d *CLIDriver) Write(logEntry p.LogEntry) {
-	fmt.Printf("[%s] %s: %s | Tags: %v\n",
-		logEntry.Timestamp.Format("2006-01-02 15:04:05"),
+	var output strings.Builder
+
+	fmt.Fprintf(&output, "[%s] %s: %s",
+		logEntry.Timestamp.Format("2006-01-02 15:04:05.000"),
 		logEntry.Level,
 		logEntry.Message,
-		logEntry.Tags,
 	)
+
+	if len(logEntry.Tags) > 0 {
+		fmt.Fprintf(&output, " | Tags: %v", logEntry.Tags)
+	}
+
+	if logEntry.TransactionID != nil {
+		fmt.Fprintf(&output, " | TxID: %s", *logEntry.TransactionID)
+	}
+
+	fmt.Println(output.String())
 }
--- main.go
+++ main.go
@@ -1,17 +1,66 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
 	a "github.com/radugaf/PlentyTelemetry/adapters"
-	logger "github.com/radugaf/PlentyTelemetry/domain"
+	d "github.com/radugaf/PlentyTelemetry/domain"
+	p "github.com/radugaf/PlentyTelemetry/ports"
 )
+
+var logger p.LoggingService
+
+type Post struct {
+	ID     int    `json:"id,omitempty"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	UserID int    `json:"userId"`
+}
 
 func main() {
 	cliAdapter := a.NewCLIDriver()
-	logger := logger.NewLogger(cliAdapter)
-	logger.Info("Test Started", map[string]string{
-		"origin":     "http",
-		"customerId": "123",
+	logger = d.NewLogger(cliAdapter)
+
+	logger.Info("Starting API tests", map[string]string{
+		"service": "api-tester",
+		"target":  "jsonplaceholder.typicode.com",
 	})
-	logger.Debug("Debug", nil)
-	logger.Error("Error", nil)
+
+	testGetPosts()
+}
+
+func testGetPosts() {
+	txID := logger.StartTransaction()
+
+	logger.Info("Testing GET posts", map[string]string{
+		"operation": "fetch_posts",
+		"endpoint":  "/posts",
+	}, txID)
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts?_limit=1")
+	if err != nil {
+		logger.Error("Failed to fetch posts", map[string]string{
+			"error": err.Error(),
+		}, txID)
+		return
+	}
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	var posts []Post
+	json.Unmarshal(body, &posts)
+
+	logger.Info("Posts fetched successfully", map[string]string{
+		"status_code": fmt.Sprintf("%d", res.StatusCode),
+		"count":       fmt.Sprintf("%d", len(posts)),
+		"first_title": posts[0].Title,
+	}, txID)
+
+	logger.Debug("Response details", map[string]string{
+		"content_type":  res.Header.Get("Content-Type"),
+		"response_size": fmt.Sprintf("%d bytes", len(body)),
+	}, txID)
 }
